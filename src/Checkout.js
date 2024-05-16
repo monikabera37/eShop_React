c730c67b6d5d65cfbfb23872dbe6fc6c4671c2f1
monikabera37 <monikabera37contact@gmail.com>
2024-05-16 20:10:07 +0530

    checkout component modified

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 
 const Checkout = () => {
-  return (
-    <div>
-      <h1>checkout</h1>
-    </div>
-  )
+    return (
+        <div>
+            <div className="checkout">
+                <div className="checkout_left">
+                    <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="adImg" className="checkout_ad" />
+                    <div>
+                        <h2 className="checkout_title">
+                            Your Shopping Basket
+                        </h2>
+                        <CheckoutProduct />
+                    </div>
+                </div>
+                <div className="checkout_right">
+
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Checkout
